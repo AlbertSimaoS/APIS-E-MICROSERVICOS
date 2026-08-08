18f83f8a19e001cbd68775ac9f3ab2503c5fb8fc
Albert <72774907+AlbertSimaoS@users.noreply.github.com>
2026-08-07 21:56:12 -0300

    Rename class from multiplicationTable to MultiplicationTable

---
+++ BLOCO 1/MultiplicationTable.java
@@ -0,0 +1,21 @@
+package Bloco1;
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        System.out.println("======================================");
+        System.out.println("\t\t Tabuada");
+        System.out.println("======================================");
+        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite um valor: ");
+        int value = scan.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("-> " + value + " * " + i + " = " + value*i);
+        }
+
+        scan.close();
+    }
+}
