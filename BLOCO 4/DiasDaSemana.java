8afebff58782081eaf7fed6003ddd0d9d8bd862e
Albert <72774907+AlbertSimaoS@users.noreply.github.com>
2026-08-15 21:25:55 -0300

    Create DiasDaSemana.java to list days of the week

---
+++ BLOCO 4/DiasDaSemana.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+	    LinkedHashSet<String> diasDaSemana = new LinkedHashSet<>();
+	    
+	    diasDaSemana.add("Quarta");
+	    diasDaSemana.add("Segunda");
+	    diasDaSemana.add("Terça");
+	    diasDaSemana.add("Domingo");
+	    
+	    System.out.println(diasDaSemana);
+	}
+}
