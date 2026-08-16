ce4cd733567a235742be51259ef1af5833c4f4c2
Albert <72774907+AlbertSimaoS@users.noreply.github.com>
2026-08-15 21:17:01 -0300

    Rename GuessNumber class to guessNumber
    Renamed GuessNumber class to guessNumber.

--- BLOCO 1/exercicio03/GuessNumber.java
+++ BLOCO 1/exercicio03/GuessNumber.java
@@ -2,12 +2,12 @@ package Bloco1;
 
 import java.util.Scanner;
 
-public class GuessNumber {
+public class guessNumber {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        
+
         int aleatoryNumber = (int)(Math.random() * 100) + 1;
-        
+
         int number = 0, attempts = 0;
         do{
             System.out.println("Chute o valor entre 1 e 100: ");
