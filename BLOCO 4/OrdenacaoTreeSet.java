dd058042f18a25e1f58dc79a12a87af8ab32b253
Albert <72774907+AlbertSimaoS@users.noreply.github.com>
2026-08-15 21:27:10 -0300

    Add OrdenacaoTreeSet.java to demonstrate TreeSet usage

---
+++ BLOCO 4/OrdenacaoTreeSet.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+	    TreeSet<String> lista = new TreeSet<>();
+	    
+	    lista.add("Marcos");
+	    lista.add("Zico");
+	    lista.add("Albert");
+	    
+	    System.out.println(lista);
+	}
+}
